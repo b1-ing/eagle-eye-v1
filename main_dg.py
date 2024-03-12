83da3d2a509c67726646cb90c27dacd852e2a2ab
b1-ing <brendanling04@gmail.com>
2024-03-12 15:30:04 +0800

    test commit main code

--- main_dg.py
+++ main_dg.py
@@ -1,22 +1,32 @@
 import os
+#from aioshutil import rmtree
 import numpy as np
 import time
 from module.ExifData import *
 from module.EoData import *
 from module.Boundary import boundary
+from QGIS import map_viewer
 from module.BackprojectionResample import rectify_plane_parallel, createGeoTiff
 from rich.console import Console
 from rich.table import Table
 
+
 console = Console()
 
-input_folder = 'Data'
-ground_height = 0   # unit: m
-sensor_width = 6.3  # unit: mm, Mavic
+input_folder = 'image_26'
+ground_height = 0  # unit: m
+sensor_width = 6.16  # unit: mm, Mavic
 # sensor_width = 13.2  # unit: mm, P4RTK
 # sensor_width = 17.3  # unit: mm, Inspire
-epsg = 5186     # editable
+#epsg = 32652     # use projected coord system utm zone xxn/s starting with 326xx/327xx
 gsd = 0.1   # unit: m, set 0 to compute automatically
+
+
+dest_folder = 'Output'
+#if os.path.exists(dest_folder):
+    #rmtree(dest_folder)
+if not os.path.exists(dest_folder):
+    os.makedirs(dest_folder)
 
 if __name__ == '__main__':
     for root, dirs, files in os.walk(input_folder):
@@ -27,16 +37,16 @@ if __name__ == '__main__':
             filename = os.path.splitext(file)[0]
             extension = os.path.splitext(file)[1]
             file_path = root + '/' + file
-            dst = './' + filename
+            dst = '/' + filename
 
-            if extension == '.JPG' or extension == '.jpg':
+            if extension == '.jpg' or extension == '.JPG' or extension == '.png':
                 print('Georeferencing - ' + file)
                 start_time = time.time()
                 image = cv2.imread(file_path, -1)
 
+
                 # 1. Extract metadata from a image
                 focal_length, orientation, eo, maker = get_metadata(file_path)  # unit: m, _, ndarray
-
                 # 2. Restore the image based on orientation information
                 restored_image = restoreOrientation(image, orientation)
 
@@ -46,6 +56,17 @@ if __name__ == '__main__':
                 pixel_size = sensor_width / image_cols  # unit: mm/px
                 pixel_size = pixel_size / 1000  # unit: m/px
 
+                xcoord = eo[0]
+                ycoord = eo[1]
+                if xcoord>=0:
+                    utm_numerical = round((xcoord+180)/6+1)
+                else:
+                    utm_numerical = round((xcoord+180)/6)
+                if ycoord>=0:
+                    epsg = 32600
+                else:
+                    epsg = 32700
+                epsg = epsg + utm_numerical
                 eo = geographic2plane(eo, epsg)
                 opk = rpy_to_opk(eo[3:], maker)
                 eo[3:] = opk * np.pi / 180   # degree to radian
@@ -53,6 +74,7 @@ if __name__ == '__main__':
 
                 console.print(
                     f"EOP: {eo[0]:.2f} | {eo[1]:.2f} | {eo[2]:.2f} | {eo[3]:.2f} | {eo[4]:.2f} | {eo[5]:.2f}\n"
+                    f"{epsg}\n"
                     f"Focal Length: {focal_length * 1000:.2f} mm, Maker: {maker}",
                     style="blink bold red underline")
                 georef_time = time.time() - start_time
@@ -85,7 +107,7 @@ if __name__ == '__main__':
                 # 8. Create GeoTiff
                 print('Save the image in GeoTiff')
                 start_time = time.time()
-                createGeoTiff(b, g, r, a, bbox, gsd, epsg, boundary_rows, boundary_cols, dst)
+                createGeoTiff(b, g, r, a, bbox, gsd, epsg, boundary_rows, boundary_cols, dst, dest_folder)
                 # create_pnga_optical(b, g, r, a, bbox, gsd, epsg, dst)   # for test
                 write_time = time.time() - start_time
                 console.print(f"Write time: {write_time:.2f} sec", style="blink bold red underline")
@@ -105,3 +127,5 @@ if __name__ == '__main__':
                     str(round(write_time, 5)), str(round(processing_time, 5))
                 )
                 console.print(table)
+
+map_viewer(dest_folder,epsg)
